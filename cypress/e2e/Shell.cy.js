2a3bba20a1bc8ad1bb5fadde0fc2d00df62f9a3c
AnirudhReddyThumma <benny.dc009@gmail.com>
2024-09-08 18:11:05 -0400

    Added README file

--- cypress/e2e/Shell.cy.js
+++ cypress/e2e/Shell.cy.js
@@ -1,62 +1,55 @@
-describe('Shell Recharge Website', () => { 
+describe('Shell Recharge Website', () => {
 
-  it('Should open the website, click dropdown, select United Kingdom from Excel, verify redirection, type wales', () => { 
+  it('Should open the website, click dropdown, select United Kingdom from Excel, verify redirection, and type "Wales"', () => {
 
-    // Step 1: Visit the website 
-    cy.visit('https://shellrecharge.com/en');  
+    // Step 1: Visit the website
+    cy.visit('https://shellrecharge.com/en');
 
     // Assertion: Verify the website loaded by checking the presence of the international dropdown
     cy.get('.css-1txziwr.enmru2y4').should('be.visible');
 
     // Step 2: Handle any cookie banners
-    
-    
-    // cy.get('body').then(($body) => {  
-    //   if ($body.find('.cookiebanner__text button').length > 0) {  
-    //     // Assertion: Verify the cookie banner is visible
-    //     cy.get('.cookiebanner__text button').should('be.visible');
-    //     cy.get('.cookiebanner__text button:contains("Accept")').click({ force: true });   
-    //   } 
-    // }); 
+    cy.get('body').then(($body) => {
+      if ($body.find('.cookiebanner__text button').length > 0) {
+        // Assertion: Verify the cookie banner is visible
+        cy.get('.cookiebanner__text button').should('be.visible');
+        cy.get('.cookiebanner__text button:contains("Accept")').click({ force: true });
+      }
+    });
 
-    // Step 3: Click the international dropdown 
-    cy.get('.css-1txziwr.enmru2y4').click({ force: true }); 
+    // Step 3: Click the international dropdown
+    cy.get('.css-1txziwr.enmru2y4').click({ force: true });
 
     // Assertion: Verify the dropdown list is visible
     cy.get('.css-1txziwr.enmru2y4').should('be.visible');
 
-    // Step 4: Get the selected country from commands.js 
-    cy.getSelectedCountry().then((country) => { 
-      cy.contains(country).click({ force: true }); // Use the country from the command 
-
-  
+    // Step 4: Get the selected country from commands.js
+    cy.getSelectedCountry().then((country) => {
+      cy.contains(country).click({ force: true }); // Use the country from the command
     });
 
-    // Step 6: Click on a link or button that causes redirection 
+    // Step 5: Click on a link or button that causes redirection
     cy.get('.hamburger').click(); // Example of clicking a hamburger menu button
-     
-
-    
 
     // Find the My Account link and ensure it's set to open in a new tab
-    cy.get('a[href*="account.shellrecharge.com"]').should('have.attr', 'target', '_blank').then(($el) => { 
-      const url = $el.prop('href'); 
+    cy.get('a[href*="account.shellrecharge.com"]').should('have.attr', 'target', '_blank').then(($el) => {
+      const url = $el.prop('href');
 
       // Assertion: Verify the URL of the link is correct
       expect(url).to.include('account.shellrecharge.com');
 
-      cy.visit(url); // Open the link in the same tab 
-      cy.wait(5000); 
+      cy.visit(url); // Open the link in the same tab
+      cy.wait(5000);
 
       // Assertion: Verify redirection by checking the presence of the login or location field
       cy.url().should('include', 'account.shellrecharge.com');
-    }); 
+    });
 
-    // Step 7: Interact with the redirected page 
-    cy.iframe().find('input[placeholder="Search by location"]').should('be.visible').click().type('Wales'); 
+    // Step 6: Interact with the redirected page
+    cy.iframe().find('input[placeholder="Search by location"]').should('be.visible').click().type('Wales');
 
     // Assertion: Verify the input field contains the value 'Wales'
     cy.iframe().find('input[placeholder="Search by location"]').should('have.value', 'Wales');
-  }); 
+  });
 
 });
